52e85c26d24c086171a6aba1a5bcfbc9d4470850
Andy Kirk <andy.kirk@npeu.ox.ac.uk>
2020-04-08 12:31:41 +0100

    Added config and check for applicable folder.

--- imagemeta.php
+++ imagemeta.php
@@ -25,18 +25,25 @@ class plgSystemImageMeta extends JPlugin
         if (!$app->isAdmin()) {
             return; // Only run in admin
         }
-        
+
         $option = $app->input->get('option');
         if ($option != 'com_media') {
             return; // Only run in com_media
         }
-        
+
+        // Only run this in the applicable folder:
+        $folder = explode('/', $app->input->get('folder', '', 'path'));
+
+        if ($this->params->get('folder') != '' && $this->params->get('folder') != $folder[0]) {
+            return;
+        }
+
         $dir = str_replace(JPATH_ROOT, '', __DIR__);
-        
+
         $document = JFactory::getDocument();
         $document->addStyleSheet($dir . '/assets/vendor/webui-popover-1.2.19/jquery.webui-popover.min.css');
         $document->addStyleSheet($dir . '/assets/css/image-meta.css');
-        
+
         $document->addScript($dir . '/assets/vendor/showdown-1.9.0/showdown.min.js');
         $document->addScript($dir . '/assets/vendor/webui-popover-1.2.19/jquery.webui-popover.min.js');
         $document->addScript($dir . '/assets/js/image-meta.js');
